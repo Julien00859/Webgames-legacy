463d307ece68aebdf0fc14c46c54de6c88b03995
Julien CASTIAUX <julien.castiaux@gmail.com>
2017-03-20 00:05:58 +0100

    add smtp, logs, http response compression

--- server.py
+++ server.py
@@ -1,20 +1,25 @@
 import asyncio
-from datetime import timedelta
+from datetime import timedelta, datetime
 import logging
-from os.path import join as pathjoin
+from gzip import open as gzipopen
+from os.path import join as pathjoin, isfile, getmtime
+from shutil import copyfileobj
 from pytimeparse import parse as timeparse
 from sanic import Sanic
+from sanic_compress import Compress
 import uvloop
 import accounts
 import challenges
 import database as db
 import views
 
-def start(addr: str, port: int, domain: str, schemessl: bool,
+def start(addr: str, port: int, domain: str, schemessl: bool, pwdsalt: str,
           dbhost: str, dbport: int, dbuser: str, dbname: str, dbpwd: str,
-          pwdsalt: str, loglevel: str,
-          tklength: int, tkvalidity: str,
-          chlglength: int, chlgvalidity: str) -> None:
+          smtphost: str, smtpport: int, smtpuser: str, smtppwd: str, smtpssl: bool,
+          logstdout: bool, logstdoutlevel: str, logstdoutformat: str,
+          logfile: bool, logfilelevel: str, logfileformat: str,
+          logfilename: str, logfilearchive: bool,
+          tklength: int, tkvalidity: str, chlglength: int, chlgvalidity: str):
 
     # Set package constant
     accounts.token_length = tklength
@@ -23,15 +28,44 @@ def start(addr: str, port: int, domain: str, schemessl: bool,
     challenges.challenge_validity = timedelta(seconds=timeparse(chlgvalidity))
     db.salt = pwdsalt
 
-    # Init logging
-    logging.basicConfig(
-        format="%(asctime)s [%(levelname)s] <%(name)s> %(message)s",
-        level=getattr(logging, loglevel))
+    # Init logs
+    logging.root.level = logging.NOTSET
+    if logstdout:
+        stdout = logging.StreamHandler()
+        stdout.level = getattr(logging, logstdoutlevel)
+        stdout.formatter = logging.Formatter(logstdoutformat, style="{")
+        logging.root.addHandler(stdout)
+
+    if logfile:
+        logpath = pathjoin("logs", logfilename)
+        if logfilearchive and isfile(logpath):
+            archivepath = pathjoin("logs", datetime.fromtimestamp(getmtime(logpath)).strftime("%y-%m-%d_%H-%M-%S") + ".gz")  # RIP PEP8
+            with open(logpath, 'rb') as f_in:
+                with gzipopen(archivepath, 'wb') as f_out:
+                    copyfileobj(f_in, f_out)
+
+        file = logging.FileHandler(logpath, "w")
+        file.level = getattr(logging, logfilelevel)
+        file.formatter = logging.Formatter(logfileformat, style="{")
+        logging.root.addHandler(file)
+
+    if not any([logstdout, logfile]):
+        logging.root.addHandler(logging.NullHandler())
+
+
 
     # Init web server
     app = Sanic(__name__)
+
+    Compress(app)
+
     app.config.domain = domain
     app.config.schemessl = schemessl
+    app.config.smtphost = smtphost
+    app.config.smtpport = smtpport
+    app.config.smtpuser = smtpuser
+    app.config.smtppwd = smtppwd
+    app.config.smtpssl = smtpssl
 
     app.add_task(db.connect(host=dbhost, port=dbport, user=dbuser,
                             database=dbname, password=dbpwd))
--- views.py
+++ views.py
@@ -6,37 +6,49 @@ from os import sep
 from secrets import compare_digest
 from smtplib import SMTP
 from email.message import EmailMessage
+from logging import getLogger
 
 import challenges
 import accounts
 
+logger = getLogger(__name__)
 template = Template(open("client" + sep + "index.html").read())
 
 async def index(req):
-    return html(template.render(name=req.args.get("name", "stranger")))
+    return html(template.render())
 
 
 async def signup(req):
     if any(map(lambda key: key not in req.json, ["name", "email", "password"])):
+        logger.debug(f"Request is {req.json} but some arguments are missing.")
         raise InvalidUsage("Missing argument")
 
     if not await db.is_user_free(req.json["name"], req.json["email"]):
+        logger.debug(f"Request is {req.json} but name or email is already taken")
         raise InvalidUsage("Username or email already taken")
 
     user = db.User(req.json["name"], req.json["email"], db.hashpwd(req.json["password"]))
 
     chlg = challenges.create_for(user)
 
+    logger.info(f"User signed up with name: {user.name} and email: {user.email}. Challenge generated: {chlg}")
     with open("mails" + sep + "challenge.txt") as mailtext:
         mail = EmailMessage()
         mail.set_content(mailtext.read().format(domain=req.app.config.domain,
                                                 scheme="https" if req.app.config.schemessl else "http",
                                                 challenge=chlg))
         mail["Subject"] = "WebGames Registration Challenge"
-        mail["From"] = "noreply@julien00859.be"
+        mail["From"] = req.app.config.smtpuser
         mail["To"] = user.email
 
-        with SMTP("localhost") as smtp:
+        with SMTP(req.app.config.smtphost, req.app.config.smtpport) as smtp:
+            if req.app.config.smtpssl:
+                smtp.ehlo()
+                smtp.starttls()
+                smtp.ehlo()
+            else:
+                smtp.helo()
+            smtp.login(req.app.config.smtpuser, req.app.config.smtppwd)
             smtp.send_message(mail)
 
     return text("Challenge sent")
@@ -44,40 +56,49 @@ async def signup(req):
 
 async def signin(req):
     if any(map(lambda key: key not in req.json, ["login", "password"])):
+        logger.debug(f"Request is {req.json} but some arguments are missing.")
         raise InvalidUsage("Missing argument")
 
-    user = db.get_user_by_login(req.json["login"])
+    user = await db.get_user_by_login(req.json["login"])
     if user is None:
+        logger.debug(f"Request is {req.json} but user coundn't be found.")
         raise NotFound("User not found")
 
     if accounts.is_locked(user.name, req.ip):
+        logger.debug(f"Request is {req.json} but the account is frozen.")
         raise InvalidUsage("Account frozen")
 
     if not compare_digest(user.password, db.hashpwd(req.json["password"])):
+        logger.debug(f"Request is {req.json} but the password is invalid.")
         unfreeze = accounts.fail(user.name, req.ip)
         raise InvalidUsage("Invalid password. Account frozen until " + unfreeze.isoformat(sep=" ", timespec="seconds"))
 
     token = accounts.register(user.name)
-
+    logger.info(f"User {user.name} connected. Token generated: {token}")
     return json({"token": token, "name": user.name})
 
 
 async def refresh(req):
     if "name" not in req.json or "token" not in req.json:
+        logger.debug(f"Request is {req.json} but some arguments are missing.")
         raise InvalidUsage("Missing argument")
 
     user = db.get_user_by_name(req.json["name"])
     if user is None:
+        logger.debug(f"Request is {req.json} but user coundn't be found.")
         raise NotFound("User not found")
 
     if accounts.is_locked(user.name, req.ip):
+        logger.debug(f"Request is {req.json} but the account is frozen.")
         raise InvalidUsage("Account frozen")
 
     if not accounts.is_valid(req.json["name"], req.json["token"]):
+        logger.debug(f"Request is {req.json} but the token is invalid or expirated.")
         accounts.fail(user.name, req.ip)
         raise NotFound("Token not found or expirated")
 
-    return json({"token": token, "name": user.name})
+    logger.info(f"User {user.name} refreshed it's token: {req.json['token']}")
+    return json({"token": req.json["token"], "name": user.name})
 
 
 async def signout(req):
@@ -90,10 +111,13 @@ async def signout(req):
 
 async def challenge(req, token):
     if challenges.is_locked(req.ip):
+        logger.debug(f"Challenge is {token} but the account is frozen.")
         raise InvalidUsage("Account frozen")
 
     if not await challenges.solve(token):
+        logger.debug(f"Challenge {token} is invalid.")
         unfreeze = challenges.fail(req.ip)
         raise InvalidUsage("Invalid token. Account frozen until " + unfreeze.isoformat(sep=" ", timespec="seconds"))
 
+    logger.info(f"Challenge {token} validated")
     return redirect(req.app.url_for("index"))
