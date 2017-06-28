70eb50a915b55f201591d861a5b0979f978b9621
Julien00859 <julien.castiaux@gmail.com>
2017-06-28 04:26:51 +0200

    better log message

--- manager/clients_handler.py
+++ manager/clients_handler.py
@@ -10,20 +10,22 @@ import jwt
 logger = getLogger(__name__)
 Ping = namedtuple("Ping", ["value", "time_sent"])
 Command = namedtuple("Command", ["restricted_to", "pattern"])
+command_re = re.compile("[\w-]+\.[\w-]+\.[\w-]+ \w+( .*)?")
+commands = {
+    "quit":     Command(None, re.compile("(?P<reason>.*))?")),
+    "ping":     Command(None, re.compile("(?P<value>[0-9]+)")),
+    "pong":     Command(None, re.compile("(?P<value>[0-9]+)")),
+
+    "add":      Command("api", re.compile("(<?P<game>\S+)")),
+    "remove":   Command("api", re.compile("(<?P<game>\S+)"))
+    "enable":   Command("api", re.compile("(?P<game>\S+)")),
+    "disable":  Command("api", re.compile("(?P<game>\S+")),
+
+    "join":     Command("user", re.compile("(?P<queue>\S+")),
+    "leave":    Command("user", re.compile("(?P<queue>\S+"))
+}
 
 class ClientHandler:
-    command_re = re.compile("[\w-]+\.[\w-]+\.[\w-]+ \w+( .*)?")
-    commands = {
-        "quit":     Command(None, re.compile("(?P<reason>.*))?")),
-        "ping":     Command(None, re.compile("(?P<value>[0-9]+)")),
-        "pong":     Command(None, re.compile("(?P<value>[0-9]+)")),
-
-        "add":      Command("api", re.compile("(<?P<game>\S+)")),
-        "remove":   Command("api", re.compile("(<?P<game>\S+)"))
-        "enable":   Command("api", re.compile("(?P<game>\S+)")),
-        "disable":  Command("api", re.compile("(?P<game>\S+"))
-    }
-    
     def __init__(self, peername, sendfunc, closefunc, loop=None):
         """Initiate a new client with its socket and peername"""
         self.peername = peername
@@ -44,9 +46,9 @@ class ClientHandler:
 
     def __str__(self):
         return "{type} {name}#{id} at {host}:{port}".format(
-            self.jwt["clienttype"].title(),
-            self.jwt["username"],
-            self.jwt["_id"],
+            self.jwt["type"].title(),
+            self.jwt["name"],
+            self.jwt["id"],
             *self.peername)
 
     async def evaluate(self, data: str):
@@ -63,23 +65,22 @@ class ClientHandler:
             try:
                 self.jwt = jwt.decode(jwt_payload)
             except Exception as e:
-                logger.warning("[%s] %s", str(self), e.message)
-                self.kick(e.message)
+                await self.kick(e.message)
 
             if command not in commands
-                logger.warning("[%s] Command \"%s\" not found", line)
+                logger.warning("%s sent an unknown command: %s", str(self), line)
                 await self.send("error command \"{}\" not found\r\n".format(line))
                 continue
 
             if commands[command].restricted_to not in [None, jwt.clienttype]:
-                logger.warning("[%s] Command \"%s\" not available to %s", str(self), command, jwt.clienttype)
-                await self.send("error command \"{}\" is not available for you\r\n".format(command.name))
+                logger.warning("%s tried to use the command \"%s\"", str(self), command)
+                await self.send("error command \"{}\" is not available for you\r\n".format(command))
                 continue
             
             match = commands[command].pattern.match(args)
             if not match:
-                logger.warning("[%s] Syntax error for command \"%s\" with args: %s", str(self), command, args)
-                await self.send("error syntax error for command \"{}\" with args: {}\r\n".format(command, args))
+                logger.warning("%s has syntax error in command \"%s\" with args: %s", str(self), command, args)
+                await self.send("error syntax error in command \"{}\" with args: {}\r\n".format(command, args))
                 continue
 
             func = getattr(self, command)
@@ -88,7 +89,7 @@ class ClientHandler:
     async def send_ping(self, value=None):
         """Send a ping request to the client"""
         if self.ping is not None:
-            raise RuntimeError("Ping was already sent")
+            raise RuntimeError("Ping pending")
 
         if value is None:
             value = randint(1000, 9999)
@@ -98,7 +99,7 @@ class ClientHandler:
         self.call_laters["kick"] = self.loop.call_later(30, self.kick, "ping timeout")
 
     async def kick(self, reason: str=""):
-        logger.warning("Client %s is kicked because of: %s", str(self), reason or "-no reason given-")
+        logger.warning("%s is kicked because of: %s", str(self), reason or "-no reason given-")
         if reason:
             await self.send("close {}\r\n".format(reason))
         else:
@@ -115,14 +116,14 @@ class ClientHandler:
 
     def quit(self, reason: str=""):
         """Recieve a close request, mark the client as closed"""
-        logger.info("Client %s has closed the connexion: %s", str(self), reason or "-no reason given-")
+        logger.info("%s has closed the connexion: %s", str(self), reason or "-no reason given-")
         for call in self.call_laters.value():
             call.cancel()
         self.closed = True
 
     async def ping(self, value: int):
         """Recieve a ping request from the client, send back a pong"""
-        self.send("pong {}\r\n".format(value))
+        await self.send("pong {}\r\n".format(value))
 
     async def pong(self, value: int):
         """Recieve a pong answer from the client, validate the pong and update statistics"""
