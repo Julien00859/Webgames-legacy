b1994d4448c6b028e74bf7cff8c54bef617754f6
Mathieu-R <mathieu0709@gmail.com>
2017-06-29 15:29:48 +0200

    use and not get

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-router.get('/api/auth', require('admin'));
-router.get('/api/queue', require('queue'));
-router.get('/api/admin', require('admin'));
+router.use('/api/auth', require('admin'));
+router.use('/api/queue', require('queue'));
+router.use('/api/admin', require('admin'));
 
 http.createServer(app).listen(PORT, _ => {
   console.log(`[WebGames] Admin page running on http://localhost:${PORT}`);
