a280974eb02937ddaa91104612dd08784b653ddb
Julien00859 <julien.castiaux@gmail.com>
2017-07-04 15:14:12 +0200

    add doctest

--- manager/tools.py
+++ manager/tools.py
@@ -2,6 +2,15 @@ from typing import get_type_hints
 from functools import partial
 
 def cast_using_type_hints(type_hints: dict, kwargs: dict):
+    """Cast each value of a kwargs to the type in the function's signature
+    
+    >>> from typing import get_type_hints
+    >>> def foo(a: int, b: bool):
+    ...     pass
+    >>> kwargs = {'a': '15', 'b': "True"}
+    >>> cast_using_type_hints(get_type_hints(foo), kwargs)
+    {'a': 15, 'b': True}
+    """
     return {key: None if value is None else type_hints[key](value) for key, value in kwargs.items()}
 
 def cast_list_using_type_hints(func, datas: list):
@@ -13,3 +22,7 @@ async def call_later_coro(delay, coro, *args, **kwargs):
     await asyncio.sleep(delay)
     logger.debug("Call %s with args %s %s", coro, args, kwargs)
     await coro(*args, **kwargs)
+
+if __name__ == "__main__":
+    from doctest import testmod
+    testmod()
