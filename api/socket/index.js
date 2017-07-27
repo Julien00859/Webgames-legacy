2ca8ffe485485b813804996ac1cdab38a74644ad
Mathieu-R <mathieu0709@gmail.com>
2017-07-27 17:18:24 +0200

    remove ugly while loop. Now retry works

--- api/socket/index.js
+++ api/socket/index.js
@@ -2,21 +2,20 @@ const net = require('net');
 const {MANAGER_HOST, MANAGER_PORT} = require("../config");
 
 const socket = new net.Socket();
-const isConnected = false;
 
-while(!isConnected) {
-  try { 
-    setTimeout(_ => socketConnection(), 1000);
-    isConnected = true;
-  }
-  catch(_) {}
-}
+socketConnection();
 
 function socketConnection() {
   socket.connect(MANAGER_PORT, MANAGER_HOST, _ => {
     console.log(`[WebGames] Connected to the Queue Manager.`);
   });
 }
+
+socket.on('error', err => {
+  if (err.code === 'ECONNREFUSED') {
+    setTimeout(_ => socketConnection(), 2000);
+  }
+});
 
 socket.on('end', _ => console.log('[WebGames] Queue Manager disconnected.'));
 
