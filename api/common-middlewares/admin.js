1fee16df248cb2e5ee5117dfdf153c7605db6657
Mathieu-R <mathieu0709@gmail.com>
2017-07-31 10:13:28 +0200

    log debug

--- api/common-middlewares/admin.js
+++ api/common-middlewares/admin.js
@@ -1,11 +1,13 @@
 module.exports = (req, res, next) => {
   //console.log(req);
   if (!req.user) {
+    console.log('no user');
     res.status(401).send('you should login !');
     return;
   }
 
   if (!req.user.admin && !req.user.type === 'manager') {
+    console.log('admin or manager only !!');
     res.status(401).send('admin or manager only !');
     return;
   }
