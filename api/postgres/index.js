c7ef80a90039d4149682806c04c0cc0035d34eab
Julien00859 <julien.castiaux@gmail.com>
2017-08-17 23:17:18 +0200

    remove node webapi

--- api/postgres/index.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('sequelize');
-const {POSTGRES_HOST, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB} = require("../config");
-const db = new sequelize(POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, {
-  host: POSTGRES_HOST,
-  dialect: 'postgres',
-  logging: false
-});
-
-module.exports = db;
